d0b67dfcfef71136107b30ac417d69883870ebc6
pauytrh2 <pauytrh@gmail.com>
2026-05-07 09:06:04 +0300

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourDirControl.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="FourDirControl", group="Linear OpMode")
+public class FourDirControl extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("left_front");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("left_back");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("right_front");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("right_back");
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y * 0.5;
+            double x = gamepad1.left_stick_x * 1.1 * 0.5;
+            double rx = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+        }
+    }
+}
