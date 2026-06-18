64f149b811cf31ba45022eee2a9e5c061c8e40ad
pauytrh <pauytrh@disroot.org>
2026-06-18 12:05:02 +0300

    Make work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/A_ParentsDay.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/A_ParentsDay.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.SubSystems.CalcPower;
 import org.firstinspires.ftc.teamcode.SubSystems.IMUInit;
 import org.firstinspires.ftc.teamcode.SubSystems.Motors;
 
@@ -15,6 +16,11 @@ public class A_ParentsDay extends LinearOpMode {
     @Override
     public void runOpMode() {
         DcMotor[] motors = Motors.initMotors(hardwareMap);
+        DcMotor frontLeftMotor = motors[0];
+        DcMotor backLeftMotor = motors[1];
+        DcMotor frontRightMotor = motors[2];
+        DcMotor backRightMotor = motors[3];
+
         IMU imu = IMUInit.GetIMU(hardwareMap);
 
         telemetry.addData("Status", "Initialized & Ready");
@@ -22,33 +28,16 @@ public class A_ParentsDay extends LinearOpMode {
 
         waitForStart();
 
-        if (isStopRequested()) return;
-
         if (opModeIsActive()) {
-            imu.resetYaw();
-
-            Motors.setPower(motors, new double[]{0.2, 0.2, 0.2, 0.2});
-            Sleep(1);
-
-            double targetAngle = 90;
-            while (!(Math.abs(GetSpin(imu, targetAngle)[0]) < 2) && opModeIsActive()) {
-                double[] spin = GetSpin(imu, targetAngle);
-                double error = spin[0];
-                double targetSpeed = spin[1];
-
-                telemetry.addData("Target Angle", targetAngle);
-                telemetry.addData("Target Speed", targetSpeed);
-                telemetry.addData("Error", error);
-                telemetry.update();
-
-                Motors.setPower(motors, new double[]{targetSpeed, targetSpeed, -targetSpeed, -targetSpeed});
-            }
-
+            Motors.setPower(motors, CalcPower.GetPower(imu, 0, 0.5, 0));
+            SleepMil(2000);
             Motors.setPower(motors, new double[]{0, 0, 0, 0});
+
+//          Spin(imu, motors);
         }
     }
 
-    public static void Sleep(long timeout) {
+    public static void Sleep(int timeout) {
         try {
             TimeUnit.SECONDS.sleep(timeout);
         } catch (InterruptedException e) {
@@ -56,13 +45,38 @@ public class A_ParentsDay extends LinearOpMode {
         }
     }
 
-    public static double[] GetSpin(IMU imu, double targetAngle) {
-        imu.resetYaw();
+    public static void SleepMil(long timeout) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(timeout);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 
-        double currentAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
-        double error = targetAngle - currentAngle;
-        double target_speed = (error / 180) * 2;
+    public static void Spin(IMU imu, DcMotor[] motors) {
+        for (int i = 0; i < 4; i = i + 1) {
+            imu.resetYaw();
 
-        return new double[]{error, target_speed};
+            double targetAngle = 90;
+            double currentAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+            double error = targetAngle - currentAngle;
+
+            while (!(Math.abs(error) < 2)) {
+                currentAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+                error = targetAngle - currentAngle;
+
+                double target_speed = (error / 180) * 2;
+
+                Motors.setPower(motors, new double[]{target_speed, target_speed, -target_speed, -target_speed});
+            }
+
+            Motors.setPower(motors, new double[]{0, 0, 0, 0});
+
+            Motors.setPower(motors, new double[]{0.1, 0.1, 0.1, 0.1});
+
+            Sleep(1);
+
+            Motors.setPower(motors, new double[]{0, 0, 0, 0});
+        }
     }
 }
