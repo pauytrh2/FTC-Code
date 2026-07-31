206500d83f0df7a876c300f02be89c58ced869ef
thearmadiliking <roi.steinitz@iasa-school.org.il>
2026-07-31 12:49:59 +0300

    add comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Pathing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Pathing.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode.Utils;
-
+//imports if you could tell....
 import static java.lang.Math.round;
 
 import com.qualcomm.robotcore.hardware.IMU;
@@ -12,21 +12,21 @@ public class Pathing {
     public static void GoToPoint(IMU imu, double[] end) {
         double[] power;
 
-        double[] current = roundList(Pinpoint.getPosition());
-        double[] move = {end[0] - current[0], end[1] - current[1]};
+        double[] current = roundList(Pinpoint.getPosition());//idk why the f*ck dos this use "roundList" and why dos "roundList" even exist. if end is not rounded it will go on with the loop forever (current != end) and it just makes it less accurate. >:(
+        double[] move = {end[0] - current[0], end[1] - current[1]};//localize the end pos relevantly to the pp(pinpoint). I like this. this good. <3
 
-        while (current != end) {
-            power = CalcPower.GetPower(imu, move[0], move[1], 0);
-            Motors.setPower(power);
+        while (current != end) {// run until pinpoint is at the point
+            power = CalcPower.GetPower(imu, move[0], move[1], 0);//nononono NO!!! I hate the fact that this could work this is throwing inches at a class that expects joystick input... INCH is NOT JOYSTICK, !=, not the same, na ah. it probably would work, but it's not adjustable...
+            Motors.setPower(power);//set power to the motors
 
-            current = roundList(Pinpoint.getPosition());
+            current = roundList(Pinpoint.getPosition());//updating the parameters
             move = new double[]{end[0] - current[0], end[1] - current[1]};
         }
 
-        Motors.setPower(new double[]{0, 0, 0, 0});
+        Motors.setPower(new double[]{0, 0, 0, 0});// make sure it won't go vroom vroom kaboom
     }
 
-    public static double[] roundList(double[] list) {
+    public static double[] roundList(double[] list) {//wtf no i hate this, this has no use(either that or i'm just dumb)
         list[0] = round(list[0]);
         list[1] = round(list[1]);
 
