0a166aac8468f9aa28bb3425c2f3fbea5b650cf7
thearmadiliking <roi.steinitz@iasa-school.org.il>
2026-08-08 16:07:40 +0300

    if this fucking works..... I just flipped (dy,dx) with (dx,dy)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Pathing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Pathing.java
@@ -21,7 +21,7 @@ public class Pathing {
 
         double dx = end[0] - current[0];
         double dy = end[1] - current[1];
-        double angle = Math.atan2(dy, dx); // tha angle the robot needs to drive to get to end
+        double angle = Math.atan2(dx, dy); // tha angle the robot needs to drive to get to end
         double dist = Math.hypot(dx, dy); // calculate the distance of the robot to the endpoint
 
         while (dist >= stop_dist) { // run until pinpoint is at the end
@@ -41,7 +41,7 @@ public class Pathing {
             current = Pinpoint.getPosition(); // updating the parameters
             dx = end[0] - current[0];
             dy = end[1] - current[1];
-            angle = Math.atan2(dy, dx);
+            angle = Math.atan2(dx, dx);
             dist = Math.hypot(dx, dy);
 
             tel.update();
