264a8e741a9d6d59cca9fc011a935614883ebf5a
pauytrh <pauytrh@disroot.org>
2026-08-19 14:19:42 +0300

    Leyahliy meshaamem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/A_PPTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/A_PPTest.java
@@ -4,24 +4,26 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@Autonomous(name="A_PPTest", group="Linear OpMode")
 public class A_PPTest extends OpMode {
     public static Follower follower;
 
     private PathChain path;
 
-    private final double startX = 72;
-    private final double startY = 72;
-
-    private final Pose startPose = new Pose(startX, startY, Math.toRadians(90));
-    private final Pose topPose = new Pose(startX + 60, startY + 60, Math.toRadians(180));
-    private final Pose downPose = new Pose(startX + 60, startY + 10, Math.toRadians(0));
-    private final Pose endPose = new Pose(startX, startY, Math.toRadians(90));
+    private final Pose startPose = new Pose(0, 0, Math.toRadians(90));
+    private final Pose topPose = new Pose(40, 40, Math.toRadians(180));
+    private final Pose downPose = new Pose(40, 10, Math.toRadians(0));
+    private final Pose endPose = new Pose(0, 0, Math.toRadians(90));
 
     @Override
     public void init() {
-        follower.setStartingPose(new Pose(startX, startY));
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose());
     }
 
     @Override
@@ -32,9 +34,9 @@ public class A_PPTest extends OpMode {
                 .addPath(new BezierLine(startPose, topPose))
                 .setLinearHeadingInterpolation(startPose.getHeading(), topPose.getHeading())
                 .addPath(new BezierLine(topPose, downPose))
-                .setLinearHeadingInterpolation(topPose.getHeading(), downPose.getHeading())
+                .setTangentHeadingInterpolation()
                 .addPath(new BezierLine(downPose, endPose))
-                .setLinearHeadingInterpolation(downPose.getHeading(), endPose.getHeading())
+                .setConstantHeadingInterpolation(endPose.getHeading())
                 .build();
 
         follower.followPath(path);
